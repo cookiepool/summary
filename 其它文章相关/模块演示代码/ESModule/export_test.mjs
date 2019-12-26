bea1ca1cbe9f059381016f39143ed7eabffc67ef
cookiepool <18381317534@163.com>
2019-12-26 18:30:01 +0800

    update

--- 其它文章相关/模块演示代码/ESModule/export_test.mjs
+++ 其它文章相关/模块演示代码/ESModule/export_test.mjs
@@ -1,12 +1,26 @@
-let a = 100;
-let b = [1, 2, 3];
-let c = {
-  name: 'lee',
-  age: 25
-};
+// let a = 100;
+// let b = [1, 2, 3];
+// let c = {
+//   name: 'lee',
+//   age: 25
+// };
 
-function add(a, b) {
-  return a + b;
+// function add(a, b) {
+//   return a + b;
+// }
+
+// export {a, b, c, add};
+
+// 多个export语句
+export const a = 12;
+
+export function reduce(a, b) {
+  return a - b;
 }
 
-export {a, b, c, add};
+// export default function add(a, b) {
+//   return a + b;
+// }
+
+let j = 100;
+export default j;
