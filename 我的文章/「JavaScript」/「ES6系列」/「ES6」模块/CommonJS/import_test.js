84635205342f18ec8a7311529d2bd463b35372a6
cookiepool <cookiepool@163.com>
2022-01-08 14:38:41 +0800

    delete

--- 我的文章/「JavaScript」/「ES6系列」/「ES6」模块/CommonJS/import_test.js
+++
@@ -1,6 +0,0 @@
-const obj = require('./export_test.js');
-
-console.log(obj.a); // 100
-console.log(obj.b); // [1, 2, 3]
-console.log(obj.c); // {name: 'lee',age: 25}
-console.log(obj.add(2, 3));  // 5
